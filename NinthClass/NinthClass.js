c2917c940b7b9e43376b8d28b3032f72519e1929
pepe199204 <pepe199204@gmail.com>
2020-06-18 22:17:59 -0500

    a little advance of Pc at night

--- NinthClass/NinthClass.js
+++ NinthClass/NinthClass.js
@@ -7,12 +7,37 @@ class Bill {
 var boxATM = [];
 var delivered = [];
 
-boxATM.push(new Bill(50,3));
-boxATM.push(new Bill(20,2));
-boxATM.push(new Bill(10,2));
-var money = 210;
+boxATM.push(new Bill(50,10));
+boxATM.push(new Bill(20,30));
+boxATM.push(new Bill(10,10));
+var money = 0;
 var div = 0;
 var papers = 0;
 
 var getBtn = document.getElementById("get");
 getBtn.addEventListener("click", deliveryMoney);
+
+
+function deliveryMoney(){
+  getMoney = document.getElementById("Money");
+  for(var bi of boxATM)
+  {
+    if(money > 0)
+    {
+      div = Math.floor(money / bi.value);
+      if(div > bi.quantity){
+          papers = bi.quantity;
+
+      }else{
+        papers = div;
+      }
+      delivered.push(new Bill(bi.value,papers));
+      money = money - (bi.value * papers);
+    }
+  }
+  if(money > 0)
+  {
+      console.log("no cash");
+  }
+  console.log(delivered);
+}
