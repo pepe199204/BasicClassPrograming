13bdefe44e29e683ff6cec313de56d5cf9f6a6ad
pepe1499204 <pepe199204@gmail.com>
2020-06-18 21:37:32 -0500

    update el finquero, 18/06/2020 day

--- NinthClass/NinthClass.js
+++ NinthClass/NinthClass.js
@@ -1,1 +1,18 @@
+class Bill {
+  constructor(valueP, quantityP) {
+    this.value = valueP;
+    this.quantity = quantityP;
+  }
+}
+var boxATM = [];
+var delivered = [];
 
+boxATM.push(new Bill(50,3));
+boxATM.push(new Bill(20,2));
+boxATM.push(new Bill(10,2));
+var money = 210;
+var div = 0;
+var papers = 0;
+
+var getBtn = document.getElementById("get");
+getBtn.addEventListener("click", deliveryMoney);
